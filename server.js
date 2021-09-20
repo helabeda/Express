0c5339914a68eb01984dacd2c8758eab74af59c7
Hela <abadahela@gmail.com>
2021-09-20 15:28:11 +0100

    modif

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ const path=require('path')
 const app = express()
 const port = 5000
 const workinghours=require('./workingTime/workingTime')
-
-app.use(workinghours)
 app.use(express.static(path.join(__dirname, "workingTime")));
 
-app.get('/', (req, res) => res.sendfile("./view/index.html", {root:__dirname}))
-app.get('/services', (req, res) => res.sendfile("./view/services.html", {root:__dirname}))
-app.get('/contact', (req, res) => res.sendfile("./view/contact.html", {root:__dirname}))
+
+app.use(workinghours)
+app.use(express.static(path.join(__dirname, "view")));
+
+app.get('/', (req, res) => res.sendFile("./view/index.html", {root:__dirname}))
+app.get('/services', (req, res) => res.sendFile("./view/services.html", {root:__dirname}))
+app.get('/contact', (req, res) => res.sendFile("./view/contact.html", {root:__dirname}))
 app.use((req, res) => {
-    res.status(400).sendfile("./view/notFound.html", {root:__dirname})
+    res.status(400).sendFile("./view/notFound.html", {root:__dirname})
 })
 app.listen(port, () => console.log(`App listening on port port!`))
--- workingTime/workingTime.js
+++ workingTime/workingTime.js
@@ -1,7 +1,8 @@
 const workinghours = (req, res, next) => {
     let date = new Date();
     let days = date.getDay();
-    let hours = date.getHours();
+    let hours = date.getHours();1
+    // if(true){
     if (days == 0 || days == 5 || hours < 9 || hours > 17) {
       res.sendFile("workingTime.html", { root: __dirname });
     } else {
